5162021696143bfb0eacc324fe0bd16709d58c13
Brighton Vino Jegarajan <me@brightonvino.com>
2017-08-14 20:10:16 -0700

    Disabling Icon

--- background.js
+++ background.js
@@ -3,3 +3,34 @@ chrome.runtime.onMessage.addListener(function (msg, sender) {
         chrome.pageAction.show(sender.tab.id);
     }
 });
+
+chrome.tabs.onActivated.addListener(function (info) {
+    chrome.tabs.get(info.tabId, function (change) {
+        if (change.url == undefined) {
+            chrome.browserAction.setPopup({ tabId: info.tabId, popup: '' });
+            chrome.browserAction.setIcon({ path: 'icon-disabled.png', tabId: info.tabId });
+        }
+        else if (change.url.match(/h*\/*.facebook\.com*/) == null) {
+            chrome.browserAction.setPopup({ tabId: info.tabId, popup: '' });
+            chrome.browserAction.setIcon({ path: 'icon-disabled.png', tabId: info.tabId });
+        }
+        else {
+            chrome.browserAction.setPopup({ tabId: info.tabId, popup: 'popup.html' });
+            chrome.browserAction.setIcon({ path: 'icon.png', tabId: info.tabId });
+        }
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
+    if (tab.url == undefined) {
+        return;
+    }
+    else if (tab.url.match(/h*\/*.facebook\.com*/) == null) {
+        chrome.browserAction.setPopup({ tabId: tabId, popup: '' });
+        chrome.browserAction.setIcon({ path: 'icon-disabled.png', tabId: tabId });
+    }
+    else {
+        chrome.browserAction.setPopup({ tabId: tabId, popup: 'popup.html' });
+        chrome.browserAction.setIcon({ path: 'icon.png', tabId: tabId });
+    }
+});
